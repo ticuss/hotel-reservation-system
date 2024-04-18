2c38fc2ae75d4637cc959be352b6da9adf2bd4d5
Constantin Bogdanas <constantinbogdanas@Constantins-MacBook-Pro.local>
2024-04-18 16:51:26 +0200

    Add database layer

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,40 @@
 package api
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/ticuss/hotel-reservation-system/db"
 	"github.com/ticuss/hotel-reservation-system/types"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	ctx := context.Background()
+
+	user, err := h.userStore.GetUserByID(ctx, id)
+	fmt.Println(user)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "James",
 		LastName:  "Foo",
 	}
 	return c.JSON(u)
-}
-
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"msg": "James Foo"})
 }
--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/ticuss/hotel-reservation-system/api"
+	"github.com/ticuss/hotel-reservation-system/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const (
+	dburi    = "mongodb://localhost:27017"
+	dbname   = "hotel-reservation-system"
+	userColl = "users"
+)
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5001", "The server listen address")
 	flag.Parse()
-	app := fiber.New()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+	app := fiber.New(config)
 	apiv1 := app.Group("api/v1")
 
 	app.Get("/foo", handleFoo)
-	apiv1.Get("/users", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/users", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
